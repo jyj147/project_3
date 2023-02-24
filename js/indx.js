de29661519831e81a7ded8b7df948a0d7ef8b72f
jyj147 <jyj06789@gmail.com>
2023-02-24 19:06:57 +0900

    T

--- js/indx.js
+++ js/indx.js
@@ -1,165 +1,197 @@
 
-// $(function() {
+$(document).ready(function() {
 
-//     $.ajax({
-//         method: "GET",
-//         url: "https://dapi.kakao.com/v3/search/book?target=title",
-//         data: { query: "love" },
-//         headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
-//       })
-//         .done(function (msg) {
-//           console.log(msg);
-//           // for문 (8개)
-//           var divs = document.getElementsByTagName('div');
+    $.ajax({
+        method: "GET",
+        url: "https://dapi.kakao.com/v3/search/book?target=title",
+        data: { query: "love" },
+        headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
+      })
+        .done(function (msg) {
+          console.log(msg);
+          // for문 (8개)
+          var divs = document.getElementsByTagName('div');
     
-//           for (var i = 0; i < divs.length; i++) {
+          for (var i = 0; i < divs.length; i++) {
     
-//             $(".sg").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
-//             $(".st").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
-//             $(".st").eq(i).append("<h6>" + msg.documents[i].authors + "</h6>");
+            $(".sg").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
+            $(".st").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+            $(".st").eq(i).append("<h6>" + msg.documents[i].authors + "</h6>");
     
-//             var str = msg.documents[i].contents;
-//             var str2 = str.substring(0, 60);
+            var str = msg.documents[i].contents;
+            var str2 = str.substring(0, 60);
     
-//             $(".st").eq(i).append("<p>" + str2 + "</p>");
-//           }
-//         });
-
-// });
+            $(".st").eq(i).append("<p>" + str2 + "</p>");
+          }
+        });
 
 
-// $(function() {
-
-//     $.ajax({
-//         method: "GET",
-//         url: "https://dapi.kakao.com/v3/search/book?target=title",
-//         data: { query: "a detective story" },
-//         headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
-//       })
-//         .done(function (msg) {
-//           console.log(msg);
-//           // for문 (8개)
-//           var divs = document.getElementsByTagName('div');
-    
-//           for (var i = 0; i < divs.length; i++) {
-    
-//             $(".pk").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
-//             $(".pt").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
-//             $(".pt").eq(i).append("<h6>" + msg.documents[i].authors + "</h6>");
-    
-//             var str = msg.documents[i].contents;
-//             var str2 = str.substring(0, 60);
-//             $(".pt").eq(i).append("<p>" + str2 + "</p>");
-//           }
-//         });
-
-// });
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: "a detective story" },
+            headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
+          })
+            .done(function (msg) {
+              console.log(msg);
+              // for문 (8개)
+              var divs = document.getElementsByTagName('div');
+        
+              for (var i = 0; i < divs.length; i++) {
+        
+                $(".pk").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
+                $(".pt").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+                $(".pt").eq(i).append("<h6>" + msg.documents[i].authors + "</h6>");
+        
+                var str = msg.documents[i].contents;
+                var str2 = str.substring(0, 60);
+                $(".pt").eq(i).append("<p>" + str2 + "</p>");
+              }
+            });
 
 
-// $(function() {
+            $.ajax({
+                method: "GET",
+                url: "https://dapi.kakao.com/v3/search/book?target=title",
+                data: { query: "Spare" },
+                headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
+              })
+                .done(function (msg) {
+                  console.log(msg);
+                  // for문 (8개)
+                  var divs = document.getElementsByTagName('div');
+            
+                  for (var i = 0; i < divs.length; i++) {
+            
+                    $(".Hdt").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+                    $(".Hdtt").eq(i).append("<h6>" + msg.documents[i].price + "</h6>");
+            
+                    var str = msg.documents[i].contents;
+                    var str2 = str.substring(0, 60);
+            
+                    $(".Hdttt").eq(i).append("<p>" + str + "</p>");
+            
+                  }
+            
+                });
+        $.ajax({
+                method: "GET",
+                url: "https://dapi.kakao.com/v3/search/book?target=title",
+                data: { query: "ballet" },
+                headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
+              })
+                .done(function (msg) {
+                  console.log(msg);
+                  // for문 (8개)
+                  var divs = document.getElementsByTagName('div');
+            
+                  for (var i = 0; i < divs.length; i++) {
+            
+                    $(".BOOKK").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
+                    $(".BK").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+                    $(".BKt").eq(i).append("<h6>" + msg.documents[i].price + "</h6>");
+            
+                    var str = msg.documents[i].contents;
+                    var str2 = str.substring(0, 60);
+            
+                  }
+            
+                });
 
-//     $.ajax({
-//         method: "GET",
-//         url: "https://dapi.kakao.com/v3/search/book?target=title",
-//         data: { query: "Spare" },
-//         headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
-//       })
-//         .done(function (msg) {
-//           console.log(msg);
-//           // for문 (8개)
-//           var divs = document.getElementsByTagName('div');
-    
-//           for (var i = 0; i < divs.length; i++) {
-    
-//             $(".Hdt").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
-//             $(".Hdtt").eq(i).append("<h6>" + msg.documents[i].price + "</h6>");
-    
-//             var str = msg.documents[i].contents;
-//             var str2 = str.substring(0, 60);
-    
-//             $(".Hdttt").eq(i).append("<p>" + str + "</p>");
-    
-//           }
-    
-//         });
+        $.ajax({
+                method: "GET",
+                url: "https://dapi.kakao.com/v3/search/book?target=title",
+                data: { query: "ballet" },
+                headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
+              })
+                .done(function (msg) {
+                  console.log(msg);
+                  // for문 (8개)
+                  var divs = document.getElementsByTagName('div');
+            
+                  for (var i = 0; i < divs.length; i++) {
+            
+                    $(".WNi").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
+                    $(".WNit").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+                    $(".WP").eq(i).append("<h6>" + msg.documents[i].price + "</h6>");
+            
+                    var str = msg.documents[i].contents;
+                    var str2 = str.substring(0, 60);
+            
+                  }
+            
+                });
 
-// });
+});
 
+// $.ajax({
+//     method: "GET",
+//     url: "https://dapi.kakao.com/v3/search/book?target=title",
+//     data: { query: "부" },
+//     headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
+// })
 
-// $(function() {
+//     .done(function (msg) {
+//         var boxs = document.getElementsByClassName('sg');
+//         for (var i = 0; i < boxs.length; i++) {
+//             var str = msg.documents[i].title;
+//             var str3 = msg.documents[i].price;
 
-//     $.ajax({
-//         method: "GET",
-//         url: "https://dapi.kakao.com/v3/search/book?target=title",
-//         data: { query: "ballet" },
-//         headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
-//       })
-//         .done(function (msg) {
-//           console.log(msg);
-//           // for문 (8개)
-//           var divs = document.getElementsByTagName('div');
-    
-//           for (var i = 0; i < divs.length; i++) {
-    
-//             $(".BOOKK").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
-//             $(".BK").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
-//             $(".BKt").eq(i).append("<h6>" + msg.documents[i].price + "</h6>");
-    
-//             var str = msg.documents[i].contents;
-//             var str2 = str.substring(0, 60);
-    
-//           }
-    
-//         });
-
-// });
-
-
-
-
-// $(function() {
-
-//     $.ajax({
-//         method: "GET",
-//         url: "https://dapi.kakao.com/v3/search/book?target=title",
-//         data: { query: "ballet" },
-//         headers: { Authorization: "KakaoAK 7e55dc837ebf9856b2d84e9936a60473" }
-//       })
-//         .done(function (msg) {
-//           console.log(msg);
-//           // for문 (8개)
-//           var divs = document.getElementsByTagName('div');
-    
-//           for (var i = 0; i < divs.length; i++) {
-    
-//             $(".WNi").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
-//             $(".WNit").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
-//             $(".WP").eq(i).append("<h6>" + msg.documents[i].price + "</h6>");
-    
-//             var str = msg.documents[i].contents;
-//             var str2 = str.substring(0, 60);
-    
-//           }
-    
-//         });
-
-// });
-
-
-
-
-
-
-//     $(function() {
-//         function slide() {
-//             $('.slide').stop().animate({ marginLeft: "-100%" }, 800, function () {
-//               $('.slide li:first').appendTo('.slide');
-//               $('.slide').css({ marginLeft: 0 });
-//             });
-//           }
-      
-//         setInterval(slide, 3000);
-
+//             var str2 = str.substring(0, 20);
+//             $(".sg").eq(i).append('<a href="#">' + "<img src='" + msg.documents[i].thumbnail + "'/>" + "</a>")
+//                 .append("<h3>" + '<a href="#">' + str2 + "</a>" + "</h3>")
+//                 .append("<h5>" + msg.documents[i].authors + "</h5>")
+//                 .append("<h6>" + str3 + "</h6>");
+//         }
 //     });
+    
+
+
+
+// $(function() {
+
+    
+
+// });
+
+
+// $(function() {
+
+    
+
+// });
+
+
+// $(function() {
+
+    
+
+// });
+
+
+
+
+// $(function() {
+
+    
+
+// });
+
+
+
+
+
+
+    $(function() {
+        function slide() {
+            $('.slide').stop().animate({ marginLeft: "-100%" }, 800, function () {
+              $('.slide li:first').appendTo('.slide');
+              $('.slide').css({ marginLeft: 0 });
+            });
+          }
+      
+        setInterval(slide, 3000);
+
+    });
         
     
